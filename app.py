8a7394a4adeb6565eb1164daad5813382bd6b395
MiniProject_VedikaIngle <vdkingle11@gmail.com>
2026-02-05 22:53:20 +0530

    Fix Render deployments - bind to 0.0.0.0

--- app.py
+++ app.py
@@ -169,7 +169,10 @@ def ip_info():
     return resp, 500
 
 
+# if __name__ == "__main__":
+#   port = int(os.environ.get("PORT", 5000))
+#   app.run(host="127.0.0.1", port=port, debug=True)
+
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
-  app.run(host="127.0.0.1", port=port, debug=True)
-
+  app.run(host="0.0.0.0", port=port, debug=False)
